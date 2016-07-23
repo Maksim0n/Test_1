8afd13c1b4c2441a680620e547c098f1a05dcb91
НОУТ <ryazanov.maksim@gmail.com>
2016-07-23 15:03:30 +0600

    Проверка!

---
+++ Shapes/src/ru/aITSchool/Maksim0n/shapes/Main.java
@@ -0,0 +1,10 @@
+package ru.aITSchool.Maksim0n.shapes;
+
+/**
+ * Created by НОУТ on 23.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+    }
+}
